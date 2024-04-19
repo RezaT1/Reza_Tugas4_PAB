6c0a5f8975bfd518a698fbe7d756212baab5df9e
Reza Trio Nurdiansyah <nurdiansyahr6@gmail.com>
2024-04-19 20:21:13 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Authentication PAB
+
+This repo was made to fulfill task on my study, so you can ignore this repo because this repo are not very interesting.
+
+## To-do
+
+- [x] Scaffolding project
+- [x] Make fragment (still temp!)
+- [x] Slicing interface
+- [x] Integrate intent for passing data from EditText text
---
+++ app/src/main/java/com/nenofetch/authenticationpagepab/MainActivity.java
@@ -0,0 +1,55 @@
+package com.nenofetch.authenticationpagepab;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button loginBtnFragment, registerBtnFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        loginBtnFragment = findViewById(R.id.btn_login);
+        registerBtnFragment = findViewById(R.id.btn_register);
+
+
+        loginBtnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Login());
+            }
+        });
+
+        registerBtnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new Register());
+            }
+        });
+
+    }
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_id, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
